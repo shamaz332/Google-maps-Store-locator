983ffcec8712ae0e70ce1f147905f449d5dac8d3
shamaz saeed <shamazsaeed332.com>
2020-04-10 23:32:51 +0500

    done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,161 @@
-function initMap() {
-    var lostAngeles = {
-        lat: 34.063380, 
-        lng: -118.358080};
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: lostAngeles,
-      zoom: 11,
-      mapTypeId: 'roadmap',
-      // mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-    });
-    var marker = new google.maps.Marker({
-        position: lostAngeles,
-        map: map,
-        title: 'lost Angles'
-        });
+window.onload = () => {
+
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
+initMap = () => {
+  var lostAngeles = {
+    lat: 34.063380,
+    lng: -118.358080
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: lostAngeles,
+    zoom: 11,
+    mapTypeId: 'roadmap',
+    // mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+  });
+  // var marker = new google.maps.Marker({
+  //   position: lostAngeles,
+  //   map: map,
+  //   title: 'lost Angles'
+  // });
+infoWindow = new google.maps.InfoWindow();
+
+searchStores()
+}
+
+
+
+searchStores=()=>{
+  var foundStores = [];
+  var zipCode = document.getElementById('zip-code-input').value;
+  if(zipCode){
+      for(var store of stores){
+          var postal = store['address']['postalCode'].substring(0, 5);
+          if(postal == zipCode){
+              foundStores.push(store);
+          }
+      }
+  } else {
+      foundStores = stores;
   }
+  clearLocations();
+  displayStores(foundStores);
+  showStoresMarkers(foundStores);
+  setOnClickListener();
+}
+
+function clearLocations(){
+  infoWindow.close();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;
+}
+
+function setOnClickListener(){
+  var storeElements = document.querySelectorAll('.store-container');
+  storeElements.forEach(function(elem, index){
+      elem.addEventListener('click', function(){
+          new google.maps.event.trigger(markers[index], 'click');
+      })
+  })
+}
+
+
+function displayStores(stores) {
+
+  var storeHtml = '';
+  for (var [index, store] of stores.entries()) {
+
+    var address = store['addressLines'];
+    var phone = store['phoneNumber']
+
+    storeHtml += `
+  <div class="store-container">
+  <div class="store-container-background">
+  <div class="store-info-container">
+  <div class="store-address">
+      <span> ${address[0]} </span>
+      <span>${address[1]}</span>
+
+
+  </div>
+  <div class="store-phone-number">
+      ${phone}
+  </div>
+</div>
+  <div class="store-number-container">
+      <div class="store-number">
+          ${index + 1}
+      </div>
+  </div>
+  </div>
+</div>
+
+  `
+
+    document.querySelector('.stores-list').innerHTML = storeHtml
+  }
+}
+showStoresMarkers = (stores) => {
+
+  var bounds = new google.maps.LatLngBounds();
+  for (var [index, store] of stores.entries()) {
+    var latlng = new google.maps.LatLng(
+      store["coordinates"]["latitude"],
+      store["coordinates"]["longitude"])
+    var name = store["name"];
+    var address = store["addressLines"][0];
+    var openStatusText = store["openStatusText"];
+    var phoneNumber = store["phoneNumber"];
+    bounds.extend(latlng)
+    createMarker(latlng, name, address, openStatusText, phoneNumber, index + 1)
+  }
+  map.fitBounds(bounds)
+}
+
+createMarker = (latlng, name, address, openStatusText, phoneNumber, index) => {
+  var html = `
+  <div class="store-info-window">
+  <div class="store-info-name">
+${name}
+</div>
+
+  <div class="store-info-status">
+${openStatusText}
+  </div>
+
+  <div class="store-info-address">
+  <div class="circle">
+  <i class="fas fa-location-arrow"></i>
+  </div>
+${address}
+  </div>
+
+  <div class="store-info-phone">
+  <div class="circle">
+  <i class="fas fa-phone-alt"></i>
+  </div>
+${phoneNumber}
+  </div>
+  </div>
+  `
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: index.toString(),
+  });
+  google.maps.event.addListener(marker, 'click', function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+}
+
+
+
--- todo.md
+++ todo.md
@@ -6,4 +6,21 @@
 - Add the text Store Locator  (Done)
 - Add input box                (done)
 - Add Store List container      (done)
-- Add individual store container
+- Add individual store container 
+
+day 3
+
+- style individual store item in store list  (done)
+- show all the store in store list based real world data done
+- show the marker on real world store data  data
+
+
+-show basic info window     
+
+
+day 4
+
+-complete style for info window
+-open info window marker on store selec in store list
+-allow a user to search for store
+-add a beautiful transition on the hover of an individual store
